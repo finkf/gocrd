3606192ba1a581aca2c874f0af4001c9ff189b7b
flo <flo@cis.lmu.de>
2018-08-29 14:10:17 +0200

    Add TextRegion interface and flexible Find function

--- page/page.go
+++ page/page.go
@@ -22,6 +22,10 @@ var (
 	idXPath               = xmlpath.MustCompile("@id")
 	regionRefIndexedXPath = xmlpath.MustCompile("/PcGts/Page/ReadingOrder/*/RegionRefIndexed")
 	wordsXPath            = xmlpath.MustCompile("./Word")
+	// check interface types
+	_ TextRegion = Region{}
+	_ TextRegion = Line{}
+	_ TextRegion = Word{}
 )
 
 func textEquivUnicodeXPath(i int) *xmlpath.Path {
@@ -44,6 +48,14 @@ func idFromNode(node *xmlpath.Node) string {
 	return id
 }
 
+// TextRegion defines an interface for abstract
+// text regions in a PAGE-XML document.
+type TextRegion interface {
+	ID() string
+	TextEquivUnicodeAt(int) (string, bool)
+	Polygon() (Polygon, error)
+}
+
 // Page represents an open page XML file.
 type Page struct {
 	path string
@@ -64,6 +76,60 @@ func Open(path string) (Page, error) {
 	return Page{path, root}, nil
 }
 
+// Match is used to match text regions.
+// If any of the IDs is the empty string,
+// the according region is ignored.
+type Match struct {
+	RegionID, LineID, WordID string
+}
+
+func (m Match) xpath() *xmlpath.Path {
+	suffix := ""
+	if m.WordID != "" {
+		suffix = fmt.Sprintf("/Word[@id=%q]", m.WordID)
+	}
+	if m.LineID != "" {
+		suffix = fmt.Sprintf("/TextLine[@id=%q]%s", m.LineID, suffix)
+	} else if suffix != "" {
+		suffix = "/*" + suffix
+	}
+	if m.RegionID != "" {
+		suffix = fmt.Sprintf("/TextRegion[@id=%q]%s", m.RegionID, suffix)
+	} else if suffix != "" {
+		suffix = "/*" + suffix
+	}
+	return xmlpath.MustCompile("/PcGts/Page" + suffix)
+}
+
+func (m Match) find(root *xmlpath.Node) (TextRegion, bool) {
+	if i := m.xpath().Iter(root); i.Next() {
+		if m.WordID != "" {
+			return Word{i.Node(), idFromNode(i.Node())}, true
+		}
+		if m.LineID != "" {
+			return Line{i.Node(), idFromNode(i.Node())}, true
+		}
+		if m.RegionID != "" {
+			r, err := newRegion(root, i.Node())
+			if err != nil {
+				return nil, false
+			}
+			return r, true
+		}
+	}
+	return nil, false
+}
+
+func (m Match) String() string {
+	return fmt.Sprintf("{%q,%q,%q}", m.RegionID, m.LineID, m.WordID)
+}
+
+// Find searches for a given {region,line,word}-ID in the PAGE-XML
+// (IDs are assumed to be unique).
+func (p Page) Find(m Match) (TextRegion, bool) {
+	return m.find(p.root)
+}
+
 // Regions returns a slice with all RegionRefIndexed elements
 func (p Page) Regions() []Region {
 	var regions []Region
@@ -80,10 +146,10 @@ func (p Page) Regions() []Region {
 	return regions
 }
 
-// FindRegionByID returns the region with the given refID.
+// FindRegionByID returns the region with the given ID.
 func (p Page) FindRegionByID(id string) (Region, bool) {
 	for _, region := range p.Regions() {
-		if region.ID == id {
+		if region.id == id {
 			return region, true
 		}
 	}
@@ -92,7 +158,7 @@ func (p Page) FindRegionByID(id string) (Region, bool) {
 
 // Region defines a text region in the page XML file.
 type Region struct {
-	ID    string
+	id    string
 	root  *xmlpath.Node
 	index int
 }
@@ -100,17 +166,22 @@ type Region struct {
 // Lines Returns all lines in a region.
 func (r Region) Lines() []Line {
 	var lines []Line
-	for i := linesXPath(r.ID).Iter(r.root); i.Next(); {
+	for i := linesXPath(r.id).Iter(r.root); i.Next(); {
 		node := i.Node()
 		lines = append(lines, Line{node, idFromNode(node)})
 	}
 	return lines
 }
 
+// ID returns the region's ID.
+func (w Region) ID() string {
+	return w.id
+}
+
 // FindLineByID searches for a line with the given ID.
 func (r Region) FindLineByID(id string) (Line, bool) {
 	for _, line := range r.Lines() {
-		if line.ID == id {
+		if line.id == id {
 			return line, true
 		}
 	}
@@ -120,7 +191,7 @@ func (r Region) FindLineByID(id string) (Line, bool) {
 // TextEquivUnicodeAt returns the i-th TextEquiv/Unicode entry
 // (indexing is zero-based).
 func (r Region) TextEquivUnicodeAt(pos int) (string, bool) {
-	if i := regionXPath(r.ID).Iter(r.root); i.Next() {
+	if i := regionXPath(r.id).Iter(r.root); i.Next() {
 		return textEquivUnicodeXPath(pos).String(i.Node())
 	}
 	return "", false
@@ -138,7 +209,7 @@ func newRegion(root, node *xmlpath.Node) (Region, error) {
 	}
 	str, ok = regionRefXPath.String(node)
 	if ok {
-		region.ID = str
+		region.id = str
 	}
 	return region, nil
 }
@@ -146,7 +217,12 @@ func newRegion(root, node *xmlpath.Node) (Region, error) {
 // Line represents a line of text in the page XML file.
 type Line struct {
 	node *xmlpath.Node
-	ID   string
+	id   string
+}
+
+// ID returns the line's ID.
+func (w Line) ID() string {
+	return w.id
 }
 
 // TextEquivUnicodeAt returns the i-th TextEquiv/Unicode entry
@@ -168,7 +244,7 @@ func (l Line) Words() []Word {
 // FindWordByID searches for a line with the given ID.
 func (l Line) FindWordByID(id string) (Word, bool) {
 	for _, word := range l.Words() {
-		if word.ID == id {
+		if word.id == id {
 			return word, true
 		}
 	}
@@ -178,7 +254,12 @@ func (l Line) FindWordByID(id string) (Word, bool) {
 // Word represents a word on a line.
 type Word struct {
 	node *xmlpath.Node
-	ID   string
+	id   string
+}
+
+// ID returns the word's ID.
+func (w Word) ID() string {
+	return w.id
 }
 
 // TextEquivUnicodeAt returns the i-th TextEquiv/Unicode element
--- page/page_test.go
+++ page/page_test.go
@@ -1,6 +1,9 @@
 package page
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestFindRegionByRefID(t *testing.T) {
 	page, err := Open("testdata/kant_aufklaerung_1784_0020.xml")
@@ -24,8 +27,8 @@ func TestFindRegionByRefID(t *testing.T) {
 			if ok != tc.find {
 				t.Fatalf("expected ok=%t; got ok=%t", tc.find, ok)
 			}
-			if tc.find && region.ID != tc.id {
-				t.Fatalf("expected %s; got %s", region.ID, tc.id)
+			if tc.find && region.ID() != tc.id {
+				t.Fatalf("expected %s; got %s", region.ID(), tc.id)
 			}
 		})
 	}
@@ -52,7 +55,7 @@ func TestRegionTextEquivUnicode(t *testing.T) {
 				t.Fatalf("expected ok=%t; got ok=%t", tc.find, ok)
 			}
 			if tc.find && got != tc.want {
-				t.Fatalf("expected %s; got %s", region.ID, tc.want)
+				t.Fatalf("expected %s; got %s", region.ID(), tc.want)
 			}
 		})
 	}
@@ -80,8 +83,8 @@ func TestFindLineByID(t *testing.T) {
 			if tc.find != ok {
 				t.Fatalf("expected ok=%t; got ok=%t", tc.find, ok)
 			}
-			if tc.find && l.ID != tc.lineID {
-				t.Fatalf("expected %s; got %s", tc.lineID, l.ID)
+			if tc.find && l.ID() != tc.lineID {
+				t.Fatalf("expected %s; got %s", tc.lineID, l.ID())
 			}
 		})
 	}
@@ -131,8 +134,71 @@ func TestFindWordByID(t *testing.T) {
 			if tc.find != ok {
 				t.Fatalf("expected ok=%t; got ok=%t", tc.find, ok)
 			}
-			if tc.find && w.ID != tc.wordID {
-				t.Fatalf("expected %s; got %s", tc.wordID, w.ID)
+			if tc.find && w.ID() != tc.wordID {
+				t.Fatalf("expected %s; got %s", tc.wordID, w.ID())
+			}
+			if tc.find {
+				if got, _ := w.TextEquivUnicodeAt(0); got != tc.word {
+					t.Fatalf("expected %s; got %s", tc.word, got)
+				}
+			}
+		})
+	}
+}
+
+func TestFind(t *testing.T) {
+	page, err := Open("testdata/kant_aufklaerung_1784_0020.xml")
+	if err != nil {
+		t.Fatalf("got error: %v", err)
+	}
+	tests := []struct {
+		m    Match
+		find bool
+	}{
+		{Match{RegionID: "r_1_1"}, true},
+		{Match{RegionID: "invalid-region-id"}, false},
+		{Match{LineID: "tl_1"}, true},
+		{Match{LineID: "invalid-line-id"}, false},
+		{Match{WordID: "w_w1aab1b1b2b1b1ab1"}, true},
+		{Match{WordID: "invalid-word-id"}, false},
+		{Match{RegionID: "r_1_1", LineID: "tl_1"}, true},
+		{Match{RegionID: "r_2_1", LineID: "tl_2"}, true},
+		{Match{RegionID: "r_1_1", LineID: "tl_2"}, false},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%s", tc.m), func(t *testing.T) {
+			_, ok := page.Find(tc.m)
+			if tc.find != ok {
+				t.Fatalf("expected ok=%t; got ok=%t", tc.find, ok)
+			}
+		})
+	}
+}
+
+// TODO: DO-IT
+func TestRectangle(t *testing.T) {
+	page, err := Open("testdata/kant_aufklaerung_1784_0020.xml")
+	if err != nil {
+		t.Fatalf("got error: %v", err)
+	}
+	tests := []struct {
+		refID, lineID, wordID, word string
+		find                        bool
+	}{
+		{"r_1_1", "tl_1", "invalid-word-id", "", false},
+		{"r_1_1", "tl_1", "w_w1aab1b1b2b1b1ab1", "(", true},
+		{"r_2_1", "tl_2", "w_w1aab1b3b2b1b1ab1", "gewiegelt", true},
+	}
+	for _, tc := range tests {
+		t.Run(tc.refID+" "+tc.lineID, func(t *testing.T) {
+			r, _ := page.FindRegionByID(tc.refID)
+			l, _ := r.FindLineByID(tc.lineID)
+			w, ok := l.FindWordByID(tc.wordID)
+			if tc.find != ok {
+				t.Fatalf("expected ok=%t; got ok=%t", tc.find, ok)
+			}
+			if tc.find && w.ID() != tc.wordID {
+				t.Fatalf("expected %s; got %s", tc.wordID, w.ID())
 			}
 			if tc.find {
 				if got, _ := w.TextEquivUnicodeAt(0); got != tc.word {
