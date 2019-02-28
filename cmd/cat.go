5ef6251c035b09c992b434cd37684578eaca9cc7
flo <flo@cis.lmu.de>
2019-02-28 22:29:05 +0100

    Reimplement cat command
    The cat command now just outputs page XML files to stdout.  There will
    be a new zip command to zip two outputs together.

--- cmd/cat.go
+++ cmd/cat.go
@@ -27,131 +27,131 @@ If both input files and input file groups are given, first the input
 files, then the input file groups are handled.`,
 	RunE: runCat,
 }
-var (
-	catLevel    string
+
+var catArgs struct {
+	iffs, ifgs  []string // input files and input file groups
+	level, mets string
 	printHeader bool
-)
+}
 
 func init() {
+	catCMD.PersistentFlags().StringVarP(
+		&catArgs.mets, "mets", "m", "mets.xml", "path to the METS file")
+	catCMD.PersistentFlags().StringArrayVarP(
+		&catArgs.ifgs, "input-file-grp", "I", nil, "input file groups")
 	catCMD.Flags().StringVarP(
-		&catLevel, "level", "l", "line", "set level of output regions [region|line|word]")
+		&catArgs.level, "level", "l", "line", "set level of output regions [region|line|word]")
 	catCMD.Flags().BoolVarP(
-		&printHeader, "header", "H", false, "ouput region id header")
+		&catArgs.printHeader, "header", "H", false, "ouput region id header")
 }
 
 func runCat(cmd *cobra.Command, args []string) error {
-	if !levelOK() {
-		return fmt.Errorf("invalid level: %s", catLevel)
+	if err := checkLevel(); err != nil {
+		return err
 	}
-	if !inputGroupsHaveLen2() {
-		return fmt.Errorf("expected two input file groups (%d given)", len(inputFileGroups))
-	}
-	return cat(os.Stdout, catArgs{
-		mets:        metsFile,
-		level:       catLevel,
-		ifg1:        inputFileGroups[0],
-		ifg2:        inputFileGroups[1],
-		printHeader: printHeader,
-	})
+	catArgs.iffs = args
+	return cat(os.Stdout)
 }
 
-type catArgs struct {
-	mets, level, ifg1, ifg2 string
-	printHeader             bool
+func cat(out io.Writer) error {
+	for _, iff := range catArgs.iffs {
+		if err := catInputFile(out, iff); err != nil {
+			return fmt.Errorf("input file %s: %v", iff, err)
+		}
+	}
+	for _, ifg := range catArgs.ifgs {
+		if err := catInputFileGroup(out, ifg); err != nil {
+			return fmt.Errorf("input file group %s: %v", ifg, err)
+		}
+	}
+	return nil
 }
 
-func cat(out io.Writer, args catArgs) error {
-	m, err := mets.Open(args.mets)
+func catInputFile(out io.Writer, path string) error {
+	iff, err := os.Open(path)
 	if err != nil {
-		return fmt.Errorf("cannot open mets file: %v", err)
+		fmt.Errorf("open %s: %v", path, err)
 	}
-	ifg1 := m.Find(mets.Match{Use: args.ifg1})
-	ifg2 := m.Find(mets.Match{Use: args.ifg2})
-	err = zip(ifg1, ifg2, func(a, b mets.File) error {
-		ls1, e2 := readRegions(a.FLocat)
-		if e2 != nil {
-			return fmt.Errorf("cannot read %s: %v", a.FLocat.URL, e2)
-		}
-		ls2, e2 := readRegions(b.FLocat)
-		if e2 != nil {
-			return fmt.Errorf("cannot read %s: %v", b.FLocat.URL, e2)
-		}
-		for i := 0; i < len(ls1); i += 2 {
-			if args.printHeader {
-				fmt.Fprintf(out, "%s\n", ls1[i])
-			}
-			fmt.Fprintf(out, "%s\n%s", ls1[i+1], ls2[i+1])
-		}
-		return nil
-	})
-	return err
-}
-
-func readRegions(f mets.FLocat) ([]string, error) {
-	r, err := f.Open()
+	defer iff.Close()
+	p, err := page.Read(iff)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("read %s: %v", path, err)
 	}
-	defer r.Close()
-	p, err := page.Read(r)
+	return catRegions(out, p)
+}
+
+func catInputFileGroup(out io.Writer, ifg string) error {
+	m, err := mets.Open(catArgs.mets)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("open METS file %s: %v", catArgs.mets, err)
 	}
-	var res []string
-	for _, region := range p.Page.TextRegion {
-		if strings.ToLower(catLevel) == "region" {
-			res = append(res, region.ID, regionString(region.TextEquiv))
-			continue
+
+	flocats := m.Find(mets.Match{Use: ifg}) // wuah-haha
+	for _, file := range flocats {
+		r, err := file.FLocat.Open()
+		if err != nil {
+			return fmt.Errorf("open FLocat %s: %v", file.FLocat.URL, err)
 		}
-		for _, line := range region.TextLine {
-			if strings.ToLower(catLevel) == "line" {
-				res = append(res, line.ID, regionString(line.TextEquiv))
-				continue
-			}
-			for _, word := range line.Word {
-				res = append(res, word.ID, regionString(word.TextEquiv))
-			}
+		defer r.Close()
+		p, err := page.Read(r)
+		if err != nil {
+			return fmt.Errorf("read %s: %v", file.FLocat.URL, err)
 		}
-	}
-	return res, nil
-}
-
-func regionString(t page.TextEquiv) string {
-	if len(t.Unicode) == 0 {
-		return ""
-	}
-	return strings.Replace(t.Unicode[0], "\n", " ", -1)
-}
-
-func levelOK() bool {
-	switch strings.ToLower(catLevel) {
-	case "line":
-		return true
-	case "region":
-		return true
-	case "word":
-		return true
-	default:
-		return false
-	}
-}
-
-func inputGroupsHaveLen2() bool {
-	return len(inputFileGroups) == 2
-}
-
-func zip(a, b []mets.File, f func(mets.File, mets.File) error) error {
-	for i := 0; i < min(len(a), len(b)); i++ {
-		if err := f(a[i], b[i]); err != nil {
+		if err := catRegions(out, p); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
+func catRegions(out io.Writer, p *page.PcGts) error {
+	level := strings.ToLower(catArgs.level)
+	for _, region := range p.Page.TextRegion {
+		if level == "region" {
+			if err := catPrint(out, region.TextRegionBase); err != nil {
+				return err
+			}
+			continue
+		}
+		for _, line := range region.TextLine {
+			if level == "line" {
+				if err := catPrint(out, line.TextRegionBase); err != nil {
+					return err
+				}
+				continue
+			}
+			for _, word := range line.Word {
+				if err := catPrint(out, word.TextRegionBase); err != nil {
+					return err
+				}
+			}
+		}
 	}
-	return b
+	return nil
+}
+
+func catPrint(out io.Writer, tr page.TextRegionBase) error {
+	if catArgs.printHeader {
+		if _, err := fmt.Fprintln(out, tr.ID); err != nil {
+			return err
+		}
+	}
+	var str string
+	if len(tr.TextEquiv.Unicode) > 0 {
+		str = tr.TextEquiv.Unicode[0]
+	}
+	_, err := fmt.Fprintln(out, str)
+	return err
+}
+
+func checkLevel() error {
+	switch strings.ToLower(catArgs.level) {
+	case "line":
+		return nil
+	case "region":
+		return nil
+	case "word":
+		return nil
+	}
+	return fmt.Errorf("invalid level: %s", catArgs.level)
 }
